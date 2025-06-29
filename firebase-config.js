5bf6586e679583b49ee271fe66bc6b6bf55fb39e
hirokisato <hiroki9821@gmail.com>
2025-06-30 08:00:31 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,11 +3,11 @@ const firebaseConfig = {
   apiKey: "AIzaSyDQhJ2zMDzyJUbfyLfoZYeIYfGPbuHcayk",
   authDomain: "research-and-development-tests.firebaseapp.com",
   projectId: "research-and-development-tests",
-  storageBucket: "research-and-development-tests.appspot.com", // ← storageBucketがtypoです
+  storageBucket: "research-and-development-tests.appspot.com",
   messagingSenderId: "129922780742",
   appId: "1:129922780742:web:6d5539b103fbc3fbe34447",
   measurementId: "G-5TDL3RVSN1"
 };
 
-// Firebase v9以前（あなたの構成ならこっち！）
+// Firebase v9以前
 firebase.initializeApp(firebaseConfig);
