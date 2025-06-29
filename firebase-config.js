ece28a9243303e0a6e73b65c3a295dba1c792cc1
hirokisato <hiroki9821@gmail.com>
2025-06-30 08:11:21 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,12 @@
 // firebase-config.js
 const firebaseConfig = {
-  apiKey: "AIzaSyDQhJ2zMDzyJUbfyLfoZYeIYfGPbuHcayk",
-  authDomain: "research-and-development-tests.firebaseapp.com",
-  projectId: "research-and-development-tests",
-  storageBucket: "research-and-development-tests.appspot.com",
-  messagingSenderId: "129922780742",
-  appId: "1:129922780742:web:6d5539b103fbc3fbe34447",
-  measurementId: "G-5TDL3RVSN1"
-};
-
+    apiKey: "AIzaSyDQhJ2zMDzyJUbfyLfoZYeIYfGPbuHcayk",
+    authDomain: "research-and-development-tests.firebaseapp.com",
+    projectId: "research-and-development-tests",
+    storageBucket: "research-and-development-tests.appspot.com",
+    messagingSenderId: "129922780742",
+    appId: "1:129922780742:web:6d5539b103fbc3fbe34447",
+    measurementId: "G-5TDL3RVSN1"
+  };
 // Firebase v9以前
 firebase.initializeApp(firebaseConfig);
