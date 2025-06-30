17430fa7852c433e4513f124e6d388c52f40e474
hirokisato <hiroki9821@gmail.com>
2025-06-30 18:42:02 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,3 @@
-// firebase-config.js
 const firebaseConfig = {
     apiKey: "AIzaSyDQhJ2zMDzyJUbfyLfoZYeIYfGPbuHcayk",
     authDomain: "research-and-development-tests.firebaseapp.com",
@@ -8,5 +7,4 @@ const firebaseConfig = {
     appId: "1:129922780742:web:6d5539b103fbc3fbe34447",
     measurementId: "G-5TDL3RVSN1"
   };
-// Firebase v9以前
 firebase.initializeApp(firebaseConfig);
